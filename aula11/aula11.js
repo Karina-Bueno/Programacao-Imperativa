098de99b44cac43293a769979596d6381f72028d
Karina-Bueno <89114089+Karina-Bueno@users.noreply.github.com>
2021-09-25 18:25:25 -0300

    Add files via upload

---
+++ aula11/aula11.js
@@ -0,0 +1,14 @@
+for (let contador = 0; contador <= 4; contador++) {
+    console.log(contador=" - Olá mundo");
+      
+}
+
+
+//Micro desafio//
+
+//tabuada//
+for (let i = 1; i <=10; i++) {
+    for (let v = 1; v <=10; v++) {
+        console.log(i +" x " + v + " = " + v * i);
+    }
+}
